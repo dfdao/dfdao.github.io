84851c8954b9077bf970167938c5a6f239f9d1eb
tony-goss <anthony.goss@tufts.edu>
2021-06-06 15:03:28 -0700

    testing w typescript

---
+++ utils/planets.ts
@@ -0,0 +1,42 @@
+export const emptyAddress = "0x0000000000000000000000000000000000000000";
+// Testing only functions
+
+export let isUnowned = (planet) => planet.owner === emptyAddress;
+
+export let isMine = (planet) => planet.owner === df.account;
+
+export let getPlanetRank = (planet) => {
+  if (!planet) return 0;
+  return planet.upgradeState.reduce((a, b) => a + b);
+};
+
+export let getSilverNeeded = (planet) => {
+  if (!planet) return 0;
+  let totalLevel = planet.upgradeState.reduce((a, b) => a + b);
+  return Math.floor((totalLevel + 1) * 0.2 * planet.silverCap);
+};
+
+export let getSilver = (planet) => {
+  if (!planet) return 0;
+  return Math.floor(planet.silver);
+};
+
+// From main client
+export let getPlanetShortHash = (planet) => {
+  if (!planet) return '00000';
+  else return planet.locationId.substring(4, 9);
+};
+
+export let getPlayerShortHash = (address) => {
+  return address.substring(0, 6);
+};
+
+export let coords = (planet) => {
+  return `(${planet.location.coords.x}, ${planet.location.coords.y})`
+}
+
+export let energyPercent = (planet) => {
+  return Math.floor(planet.energy / planet.energyCap * 100);
+}
+
+export let isAsteroid = (planet) => planet.planetResource === 1
