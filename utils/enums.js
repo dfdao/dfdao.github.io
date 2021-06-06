77c3b936c3128d5b4ade720d09ada9a15f69d645
tony-goss <anthony.goss@tufts.edu>
2021-06-06 12:40:03 -0700

    more utils

---
+++ utils/enums.js
@@ -0,0 +1,35 @@
+// Enums
+export let BiomeNames = [
+  'Unknown',
+  'Ocean',
+  'Forest',
+  'Grassland',
+  'Tundra',
+  'Swamp',
+  'Desert',
+  'Ice',
+  'Wasteland',
+  'Lava',
+];
+
+export let UpgradeBranchName = {
+  Defense: 0,
+  Range: 1,
+  Speed: 2,
+};
+
+export let SpaceType = {
+  NEBULA: 0,
+  SPACE: 1,
+  DEEP_SPACE: 2,
+};
+
+export let planetType = {
+    "PLANET": 0,
+    "ASTEROID": 1,
+    "FOUNDRY": 2,
+    "RIP": 3,
+    "QUASAR": 4
+};
+
+export let planetTypeList = ['PLANET', 'ASTEROID', 'FOUNDRY', 'RIP', 'QUASAR'];
---
+++ utils/utils.js
@@ -0,0 +1,5 @@
+// Contains all of the utilites
+
+import * as uiElts from './ui.js';
+
+export default uiElts;
