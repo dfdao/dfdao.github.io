00e5c182bc36b3aa5adb34263e1b55ed089021e6
Velorum <88809256+Velorum6@users.noreply.github.com>
2022-04-04 13:30:23 +0200

    :100:

--- lobbies.js
+++ lobbies.js
@@ -1,6 +1,5 @@
 import { lobbyScoreBoard } from "./scoreboard.js"
 
-let scoreboard = await lobbyScoreBoard()
 
 let users = [];
 let users_details = [];
@@ -23,24 +22,12 @@ const userCardTemplate = document.querySelector("[data-user-template]")
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-async function buildTable(data, tableClass) {
-    var table = document.querySelector(tableClass)
-
-    for (var i = 0; i < data.length; i++) {
-        var row = `<tr>
-                    <td>${i}</td>
-                    <td data-label="Member" style="font-size: 12px">${data[i].address}</td>
-                    <td data-label="Score">${numberWithCommas(data[i].score)}</td>
-              </tr>`
-        table.innerHTML += row
-    }
-}
 
 
-readTextFile("./lobbies.json", function(json) {
+readTextFile("./lobbies.json", async function(json) {
     var data = JSON.parse(json);
 
-    users = data.map(user => {
+    users = await data.map(user => {
         const card = userCardTemplate.content.cloneNode(true).children[0];
         card.querySelector(".card-name").textContent = user.name;
         card.querySelector(".card-address").textContent = user.address;
@@ -56,11 +43,12 @@ readTextFile("./lobbies.json", function(json) {
         }
     })
 
-    users_details = data.map(user_detail => {
+    users_details = await data.map(user_detail => {
         const detailing = document.querySelector("[details-template]").content.cloneNode(true).children[0];
         detailing.querySelector(".lobby-name").textContent = user_detail.name;
         detailing.querySelector(".lobby-address").textContent = user_detail.address;
         detailing.querySelector(".details").id = user_detail.id;
+        detailing.querySelector(".lobby-tbody").id = user_detail.id;
         document.querySelector(".detail-boxes").append(detailing);
         return {
             id: user_detail.id,
@@ -80,7 +68,18 @@ readTextFile("./lobbies.json", function(json) {
         })
 
     })
+    async function buildTable(data, tableClass) {
 
+        tableClass.innerHTML = ""
+        for (var i = 0; i < data.length; i++) {
+            var row = `<tr>
+                <td>${i}</td>
+                <td data-label="Member" style="font-size: 12px">${data[i].address}</td>
+                <td data-label="Score">${numberWithCommas(data[i].score)}</td>
+          </tr>`
+            tableClass.innerHTML += row
+        }
+    };
 
     let cardDetails = document.querySelectorAll('.card-details');
     let details = document.querySelectorAll('.details-box');
@@ -91,13 +90,26 @@ readTextFile("./lobbies.json", function(json) {
     cardDetails.forEach(x => {
         x.addEventListener('click', () => {
             const value = x.id
-            users_details.forEach(a => {
+            users_details.forEach(async a => {
                 const isVisible = a.id == value;
                 a.element.classList.toggle('hide', !isVisible);
             })
-        })
+        });
 
     })
+    cardDetails.forEach(x => {
+        x.addEventListener('click', () => {
+            const value = x.id
+            users_details.forEach(async a => {
+                if (a.id == value) {
+                    console.log(a.element.querySelector('.lobby-name'))
 
-    buildTable(scoreboard, '.lobby-tbody')
+                    a.element.querySelector(".lobby-tbody").innerHTML = "Loading"
+                    buildTable(await lobbyScoreBoard(a.address), a.element.querySelector('.lobby-tbody'));
+
+                }
+            })
+        }, { once: true });
+
+    })
 })
--- scoreboard.js
+++ scoreboard.js
@@ -1,17 +1,18 @@
 import { ethers } from 'https://cdn.skypack.dev/ethers';
 import DarkForestABI from './abi.js';
 
+export async function lobbyScoreBoard(address) {
 
-const lobbyAddress = '0x06078143f81bafcb7b5df588bf2e8247dc8702be'
-const rpcEndpoint = "https://rpc.xdaichain.com/";
-const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
-const darkforest = new ethers.Contract(lobbyAddress, DarkForestABI, provider);
 
-let player_numbers = await darkforest.getNPlayers().then(async count => {
-    return count.toNumber()
-})
+    const lobbyAddress = address
+    const rpcEndpoint = "https://rpc.xdaichain.com/";
+    const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+    const darkforest = new ethers.Contract(lobbyAddress, DarkForestABI, provider);
 
-export async function lobbyScoreBoard() {
+    let player_numbers = await darkforest.getNPlayers().then(async count => {
+        return count.toNumber()
+    })
+
     let scoreboard = await darkforest.bulkGetPlayers(0, player_numbers).then(async count => {
         let a = []
         for (let i = 0; i < count.length; i++) {
